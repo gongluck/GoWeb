712fee0ace205004ea66616d101d8bdb023934b7
gongluck <1039994845@qq.com>
2020-05-25 23:30:53 +0800

    搭建服务器_搭建服务器的方式

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
  * @Author: gongluck
  * @Date: 2020-05-25 22:50:32
  * @Last Modified by: gongluck
- * @Last Modified time: 2020-05-25 22:55:43
+ * @Last Modified time: 2020-05-25 23:30:24
  */
 
 package main
@@ -17,9 +17,30 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!", r.URL.Path)
 }
 
+type MyHandler struct{}
+
+func (m *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "通过自己创建的处理器处理请求！")
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	//http.HandleFunc("/", handler)
+
+	//myHandler := MyHandler{}
+	//http.Handle("/myHandler", &myHandler)
+
+	// 创建Server结构，并详细配置里面的字段
+	// server := http.Server{
+	// 	Addr:        ":8080",
+	// 	Handler:     &myHandler,
+	// 	ReadTimeout: 2 * time.Second,
+	// }
+	// server.ListenAndServe()
+
+	// 创建多路复用器
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
 
 	// 创建路由
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }
