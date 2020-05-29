ab972d5c330f15f4ffada84cca843a03ab4d95b4
gongluck <1039994845@qq.com>
2020-05-29 10:13:06 +0800

    数据库查询一条和查询所有

--- model/user.go
+++ model/user.go
@@ -19,9 +19,9 @@ type User struct {
 	Email    string
 }
 
-func (user *User) AddUser() error{
+func (user *User) AddUser() error {
 	sqlStr := "insert into users(username, password, email) values(?, ?, ?)"
-	
+
 	// inStmt, err := utils.Db.Prepare(sqlStr)
 	// if err != nil{
 	// 	fmt.Println("预编译异常：", err)
@@ -34,10 +34,63 @@ func (user *User) AddUser() error{
 	// }
 
 	_, err := utils.Db.Exec(sqlStr, user.Username, user.Password, user.Email)
-	if err != nil{
+	if err != nil {
 		fmt.Println("插入异常：", err)
 		return err
 	}
-	
+
 	return nil
+}
+
+func (user *User) GetUserById() (*User, error) {
+	sqlStr := "select id, username, password, email from users where id=?"
+	row := utils.Db.QueryRow(sqlStr, user.ID)
+
+	var id int
+	var username string
+	var password string
+	var email string
+	err := row.Scan(&id, &username, &password, &email)
+	if err != nil {
+		fmt.Println("查询异常：", err)
+		return nil, err
+	}
+	u := &User{
+		ID:       id,
+		Username: username,
+		Password: password,
+		Email:    email,
+	}
+	return u, err
+}
+
+func (user *User) GetUsers() ([]*User, error) {
+	sqlStr := "select id, username, password, email from users"
+	rows, err := utils.Db.Query(sqlStr)
+	if err != nil {
+		fmt.Println("查询异常：", err)
+		return nil, err
+	}
+
+	var id int
+	var username string
+	var password string
+	var email string
+	var users []*User
+	for rows.Next() {
+		err := rows.Scan(&id, &username, &password, &email)
+		if err != nil {
+			fmt.Println("查询-异常：", err)
+			return nil, err
+		}
+		u := &User{
+			ID:       id,
+			Username: username,
+			Password: password,
+			Email:    email,
+		}
+		users = append(users, u)
+	}
+
+	return users, nil
 }
--- model/user_test.go
+++ model/user_test.go
@@ -1,31 +1,49 @@
 /*
- * @Author: gongluck 
- * @Date: 2020-05-28 19:47:13 
+ * @Author: gongluck
+ * @Date: 2020-05-28 19:47:13
  * @Last Modified by: gongluck
  * @Last Modified time: 2020-05-28 20:34:01
  */
 
 package model
 
-import(
-	"testing"
+import (
 	"fmt"
+	"testing"
 )
 
-func TestMain(m *testing.M){
+func TestMain(m *testing.M) {
 	fmt.Println("开始测试用户模块：")
 	m.Run()
 }
 
-func TestAddUser(t *testing.T){
-	testAddUser(t)
+func TestAddUser(t *testing.T) {
+	t.Run("测试添加用户", testAddUser)
+	t.Run("测试查询用户", testGetUserById)
+	t.Run("测试查询所有用户", testGetUsers)
 }
 
-func testAddUser(t *testing.T){
+func testAddUser(t *testing.T) {
 	user := &User{
 		Username: "gongluck",
 		Password: "hello",
-		Email:"1039994845@qq.com",
+		Email:    "1039994845@qq.com",
 	}
 	user.AddUser()
+}
+
+func testGetUserById(t *testing.T) {
+	user := &User{
+		ID: 1,
+	}
+	u, _ := user.GetUserById()
+	fmt.Println("查询结果：", u)
+}
+
+func testGetUsers(t *testing.T) {
+	var user User
+	users, _ := user.GetUsers()
+	for k, v := range users {
+		fmt.Printf("第%v个用户是%v\n", k+1, v)
+	}
 }
